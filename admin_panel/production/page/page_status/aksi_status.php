60c5f1f90e3cd4d759e8465ed080e01fbdd07468
permadiekapermana <permadiekapermana@gmail.com>
2020-12-01 15:34:09 +0700

    CRUD Status

--- admin_panel/production/page/page_status/aksi_status.php
+++ admin_panel/production/page/page_status/aksi_status.php
@@ -6,29 +6,29 @@ $page=$_GET[page];
 $act=$_GET[act];
 
 
-if ($page=='JenisKelamin' AND $act=='input'){
+if ($page=='Status' AND $act=='input'){
 
-$jk     = $_POST['jk'];
+$status     = $_POST['status'];
 
-$query=mysql_query("INSERT INTO jenis_kelamin (jk) VALUES ('$jk')"); 
+$query=mysql_query("INSERT INTO status (status) VALUES ('$status')"); 
 header('location:../../menu.php?page='.$page);
 
 }
   
 
-elseif ($page=='JenisKelamin' AND $act=='update'){    
+elseif ($page=='Status' AND $act=='update'){    
 
-  $id_jk    = $_POST['id_jk'];
-  $jk       = $_POST['jk'];
+  $id_status    = $_POST['id_status'];
+  $status       = $_POST['status'];
   
-$query=mysql_query("UPDATE jenis_kelamin SET jk='$jk' WHERE id_jk='$id_jk'"); 							 
+$query=mysql_query("UPDATE status SET status='$status' WHERE id_status='$id_status'"); 							 
 header('location:../../menu.php?page='.$page);
 
 }
 
-elseif ($page=='JenisKelamin' AND $act=='delete'){
+elseif ($page=='Status' AND $act=='delete'){
 
-mysql_query("DELETE FROM jenis_kelamin WHERE id_jk='$_GET[id]'");
+mysql_query("DELETE FROM status WHERE id_status='$_GET[id]'");
 header('location:../../menu.php?page='.$page);
 
 }
--- admin_panel/production/page/page_status/status.php
+++ admin_panel/production/page/page_status/status.php
@@ -1,7 +1,7 @@
 <?php
 include "../config/koneksi.php";
 
-$aksi="page/page_jk/aksi_jk.php";
+$aksi="page/page_status/aksi_status.php";
 
 switch($_GET[act]){
 
@@ -14,30 +14,30 @@ default:
           <div class="col-12">
         <div class="card">
               <div class="card-header">
-                <h3 class="card-title"><a href="?page=JenisKelamin&act=tambah"><button type="button" class="btn btn-round btn-primary"><i class="fa fa-plus"></i> Tambah Data</button></a></h3>
+                <h3 class="card-title"><a href="?page=Status&act=tambah"><button type="button" class="btn btn-round btn-primary"><i class="fa fa-plus"></i> Tambah Data</button></a></h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
                   <thead>
                   <tr>
-                    <th>ID Jenis Kelamin</th>
-                    <th>Jenis Kelamin</th>
+                    <th>ID Status</th>
+                    <th>Status</th>
                     <th>Aksi</th>
                   </tr>
                   </thead>
                   <tbody>
                   <?php
-                  $tampil = mysql_query("SELECT * FROM `jenis_kelamin` ORDER BY id_jk ASC");
+                  $tampil = mysql_query("SELECT * FROM `status` ORDER BY id_status ASC");
                   $no = 1;
                   while($r=mysql_fetch_array($tampil)){                  
                   ?>
                   <tr>                  
                     <td width="17%"><?php echo"$no"; ?></td>
-                    <td><?php echo"$r[jk]"; ?></td>
+                    <td><?php echo"$r[status]"; ?></td>
                     <td width="20%">
-                    <a href='<?php echo"?page=JenisKelamin&act=edit&id=$r[id_jk]"; ?>' class='btn btn-success'><i class='fa fa-edit'></i> Edit</a>
-                    <a href="<?php echo"$aksi?page=JenisKelamin&act=delete&id=$r[id_jk]"; ?>" class="btn btn-danger" <?php echo" onClick=\"return confirm('Yakin ingin hapus data ? Data yang dihapus tidak dapat dipulihkan !')\" ";?>><i class='fa fa-trash'></i> Delete</a>
+                    <a href='<?php echo"?page=Status&act=edit&id=$r[id_status]"; ?>' class='btn btn-success'><i class='fa fa-edit'></i> Edit</a>
+                    <a href="<?php echo"$aksi?page=Status&act=delete&id=$r[id_status]"; ?>" class="btn btn-danger" <?php echo" onClick=\"return confirm('Yakin ingin hapus data ? Data yang dihapus tidak dapat dipulihkan !')\" ";?>><i class='fa fa-trash'></i> Delete</a>
                     </td>
                   </tr>
                   <?php
@@ -47,8 +47,8 @@ default:
                   </tbody>
                   <tfoot>
                   <tr>
-                    <th>ID Jenis Kelamin</th>
-                    <th>Jenis Kelamin</th>
+                    <th>ID Status</th>
+                    <th>Status</th>
                     <th>Aksi</th>
                   </tr>
                   </tfoot>
@@ -75,33 +75,33 @@ case "tambah":
             <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Tambah Jenis Kelamin</h3>
+                <h3 class="card-title">Tambah Status</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form action="<?php echo"$aksi?page=JenisKelamin&act=input"; ?>" method="POST">
+              <form action="<?php echo"$aksi?page=Status&act=input"; ?>" method="POST">
                 <div class="card-body col-md-6">
                   <div class="form-group">
-                    <label for="id_jk">ID Jenis Kelamin</label>
+                    <label for="id_status">ID Status</label>
                     <?php
-                    $query = mysql_query("SELECT max(id_jk) as idTerbesar FROM jenis_kelamin");
+                    $query = mysql_query("SELECT max(id_status) as idTerbesar FROM status");
                     $data = mysql_fetch_array($query);
                     $idTerbesar = $data['idTerbesar'];
                     $idTerbesar++;
                     ?>
-                    <input type="text" class="form-control" id="id_jk" value="<?php echo"$idTerbesar"; ?>" disabled>
-                    <input type="hidden" class="form-control" id="id_jk" name="id_jk" value="<?php echo"$idTerbesar"; ?>">
-                    <span>ID Jenis Kelamin dibuat otomatis.</span>
+                    <input type="text" class="form-control" id="id_status" value="<?php echo"$idTerbesar"; ?>" disabled>
+                    <input type="hidden" class="form-control" id="id_status" name="id_status" value="<?php echo"$idTerbesar"; ?>">
+                    <span>ID Status dibuat otomatis.</span>
                   </div>
                   <div class="form-group">
-                    <label for="jk">Jenis Kelamin</label>
-                    <input type="text" class="form-control" id="jk" name="jk" placeholder="Masukkan Jenis Kelamin" required>
+                    <label for="status">Status</label>
+                    <input type="text" class="form-control" id="status" name="status" placeholder="Masukkan Status" required>
                   </div>
                 </div>
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                    <button class="btn btn-danger" type="button" <?php echo"onclick=self.history.back()"; ?>>Cancel</button>
+                  <button class="btn btn-danger" type="button" <?php echo"onclick=self.history.back()"; ?>>Cancel</button>
                   <button type="reset" class="btn btn-warning">Warning</button>
                   <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
@@ -116,7 +116,7 @@ case "tambah":
 break;
 
 case "edit":
-$edit = mysql_query("SELECT * FROM `jenis_kelamin` WHERE id_jk='$_GET[id]'");
+$edit = mysql_query("SELECT * FROM `status` WHERE id_status='$_GET[id]'");
 $r    = mysql_fetch_array($edit);
 ?>
     <section class="content">
@@ -127,21 +127,21 @@ $r    = mysql_fetch_array($edit);
             <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Ubah Jenis Kelamin</h3>
+                <h3 class="card-title">Ubah Status</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form action="<?php echo"$aksi?page=JenisKelamin&act=update"; ?>" method="POST">
+              <form action="<?php echo"$aksi?page=Status&act=update"; ?>" method="POST">
                 <div class="card-body col-md-6">
                   <div class="form-group">
-                    <label for="id_jk">ID Jenis Kelamin</label>
-                    <input type="text" class="form-control" id="id_jk" value="<?php echo"$r[id_jk]"; ?>" disabled>
-                    <input type="hidden" class="form-control" id="id_jk" name="id_jk" value="<?php echo"$r[id_jk]"; ?>">
+                    <label for="id_status">ID Status</label>
+                    <input type="text" class="form-control" id="id_status" value="<?php echo"$r[id_status]"; ?>" disabled>
+                    <input type="hidden" class="form-control" id="id_status" name="id_status" value="<?php echo"$r[id_status]"; ?>">
                     <span>ID Jenis Kelamin tidak dapat diubah.</span>
                   </div>
                   <div class="form-group">
-                    <label for="jk">Jenis Kelamin</label>
-                    <input type="text" class="form-control" id="jk" name="jk" value="<?php echo"$r[jk]"; ?>" placeholder="Masukkan Jenis Kelamin" required>
+                    <label for="status">Status</label>
+                    <input type="text" class="form-control" id="status" name="status" value="<?php echo"$r[status]"; ?>" placeholder="Masukkan Status" required>
                   </div>
                 </div>
                 <!-- /.card-body -->
